5a1d4767a3e51101cc2102acecc19ab0717d3a9a
9Tung_Do <9Tung_Do@singaporeair.com.sg>
2025-02-20 02:01:33 +0700

    init: configs

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,21 @@
+return {
+  -- add tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      style = "moon",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
